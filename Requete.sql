d7e9bab2d070b1da05c75ee7267f4adb1c50d5a4
gaouimounir <mounir.gaoui@outlook.com>
2023-12-14 15:04:26 +0100

    feat: :sparkles: les noms, prénoms et âges des acteurs/actrices de plus de 30 ans dans l'ordre alphabétique

--- Requete.sql
+++ Requete.sql
@@ -1,3 +1,7 @@
 -----les titres et dates de sortie des films du plus récent au plus ancien----
 
 SELECT title, release_year FROM Movie ORDER BY release_year DESC;
+
+---les noms, prénoms et âges des acteurs/actrices de plus de 30 ans dans l'ordre alphabétique----
+
+SELECT firstname_actor, lastname_actor, birthdate_actor FROM Actor WHERE birthdate_actor < '1990-01-01' ORDER BY lastname_actor;
